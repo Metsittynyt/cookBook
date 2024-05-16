5d78658649a009c956acb56ce9fe84709fd4682b
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-16 17:15:13 +0300

    renamed, added log in form

--- src/pages/CreatePage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>Add new recipe</h1>
-</template>
-
-<script>
-export default {
-  name: 'CreatePage',
-};
-</script>
---
+++ src/pages/UserPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <h1>Log in</h1>
+    <LogInForm />
+  </div>
+</template>
+
+<script>
+import LogInForm from '../components/LogInForm'
+
+export default {
+  name: 'UserPage',
+  components: {
+    LogInForm
+  }
+};
+</script>
