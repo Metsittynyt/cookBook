09d173c08c64887bbaf185401f524680d92ceb3c
elvleh <elvleh@utu.fi>
2024-05-16 10:35:53 +0300

    overlay with recipeform

---
+++ src/components/CreateOverlay.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="overlay">
+        <div id="create_overlay">
+            <h1>Add new recipe</h1>
+            <button class="close_button" @click="overlay_off">x</button>
+            <RecipeForm />
+        </div>
+    </div>
+</template>
+
+<script>
+import RecipeForm from './RecipeForm'
+
+export default {
+    name: 'CreateOverlay',
+    components: {
+      RecipeForm
+    },
+    methods: {
+    overlay_off() {
+      console.log("overlay off")
+      document.querySelector(".overlay").style.display = "none";
+    }
+  }
+}
+</script>
