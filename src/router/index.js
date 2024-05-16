c6ed44c6712b7895b7d9dbb1bc2c5f3ce442eccc
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-16 17:15:48 +0300

    updated name

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import CookBookPage from '../pages/CookBookPage.vue';
-import CreatePage from '../pages/CreatePage.vue';
+import UserPage from '../pages/UserPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage, name: 'HomePage' },
     { path: '/cookbook', component: CookBookPage, name: 'CookBookPage' },
-    { path: '/create', component: CreatePage, name: 'CreatePage' }
+    { path: '/user', component: UserPage, name: 'UserPage' }
     // other routes
   ],
 });
