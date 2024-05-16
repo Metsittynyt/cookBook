1ad15c8b54e9d1c748e3dbfec0553e14ce2f24b4
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-16 17:15:34 +0300

    turned logo into link

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="topnav">
     <div class="topnavLogo">
-      <img :src="require('@/assets/photos/cookhat.png')" alt="Logo" id="Logo">
+      <router-link to="/" exact active-class="active">
+        <img :src="require('@/assets/photos/cookhat.png')" alt="Logo" id="Logo">
+      </router-link>
     </div>
     <div class="topnavLinks">
-      <router-link to="/cookbook" class="nav-item" active-class="active">My recipes</router-link>
       <router-link to="/" exact class="nav-item" active-class="active">CookBook</router-link>
+      <router-link to="/cookbook" class="nav-item" active-class="active">My recipes</router-link>
+      <router-link to="/user" class="nav-item" active-class="active">
+        <i class="fas fa-user-alt" id="user_icon"></i>
+      </router-link>
     </div>
   </div>
 </template>
