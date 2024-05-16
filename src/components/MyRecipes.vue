e8254782b616c0e1e4534400fccb7437a7da335b
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-16 15:44:16 +0300

    add edit call and public icon

--- src/components/MyRecipes.vue
+++ src/components/MyRecipes.vue
@@ -1,10 +1,14 @@
 <template>
-  <h2>My Recipes</h2>
-  <div class="MyRecipesGrid">
-    <div v-for="recipe in recipes" :key="recipe.id" class="recipe-cell">
-      <i class="fas fa-trash-alt" id="trash_can_icon" @click="delete_recipe(recipe.id)"></i>
-      <RecipeBox :recipe="recipe" />
-      <i class="fas fa-edit" id="edit_icon" @click="edit_recipe(recipe)"></i>
+  <div>
+    <h2>My Recipes</h2>
+    <div class="MyRecipesGrid">
+      <div v-for="recipe in recipes" :key="recipe.id" class="recipe-cell">
+        <i :class="['fas', 'fa-globe', recipe.public ? 'public-icon' : 'private-icon']" id="globe_icon"
+          @click="togglePublicStatus(recipe)"></i>
+        <i class="fas fa-trash-alt" id="trash_can_icon" @click="delete_recipe(recipe.id)"></i>
+        <RecipeBox :recipe="recipe" />
+        <i class="fas fa-edit" id="edit_icon" @click="edit_recipe(recipe)"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +40,18 @@ export default {
     },
     edit_recipe(recipe) {
       console.log("Edit: " + recipe.name)
+      this.$emit('edit-recipe', recipe);
+    },
+    async togglePublicStatus(recipe) {
+      try {
+        recipe.public = !recipe.public; // Toggle the public status
+        await axios.put(`http://localhost:3001/recipes/${recipe.id}`, recipe); // Send update to backend
+      } catch (error) {
+        console.error('Error updating recipe:', error);
+        this.error = 'Failed to update the recipe.';
+        // Revert the change if the request fails
+        recipe.public = !recipe.public;
+      }
     }
   },
   async mounted() {
