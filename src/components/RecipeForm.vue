a0d3227b7ab2089eb8c7d30e205bf956744d903e
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-16 14:55:50 +0300

    logic fo submitting new recipe

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="recipe-form">
-    <form @submit.prevent="submitRecipe">
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <form id="form" v-on:submit="handleSubmit" v-else>
       <div class="form-group">
         <label for="recipeName">Recipe Name:</label>
         <input type="text" id="recipeName" v-model="recipe.name" required>
@@ -13,31 +16,45 @@
         <label for="steps">Preparation Steps:</label>
         <textarea id="steps" v-model="recipe.steps" required rows="7"></textarea>
       </div>
+      <div class="form-group">
+        <label for="publicCheck">Publish?</label>
+        <input type="checkbox" id="publicCheck" v-model="recipe.public">
+      </div>
       <button type="submit">Submit Recipe</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       recipe: {
         name: '',
         ingredients: '',
-        steps: ''
+        steps: '',
+        public: false
       }
     };
   },
   name: 'RecipeForm',
   methods: {
-    submitRecipe() {
-      console.log("Submitting recipe:", this.recipe);
-      alert('Recipe submitted! Check the console for details.');
+    handleSubmit: async function (e) {
+      e.preventDefault();
+
+      try {
+        const response = await axios.post('http://localhost:3001/recipes', this.recipe)
+        console.log(response);
+      } catch (error) {
+        this.error = error;
+      }
       this.resetForm();
+      document.querySelector(".overlay").style.display = "none";
     },
     resetForm() {
-      this.recipe = { name: '', ingredients: '', steps: '' };
+      this.recipe = { name: '', ingredients: '', steps: '', public: false };
     }
   }
 }
