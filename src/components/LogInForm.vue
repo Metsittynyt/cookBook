36db179adc325063f7a5e2e3b5f31acd783e4e16
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-16 17:15:24 +0300

    form for log in

---
+++ src/components/LogInForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login_form">
+        <form id="form">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <textarea id="password" required></textarea>
+            </div>
+            <button type="submit">Log in</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LogInForm'
+}
+</script>
