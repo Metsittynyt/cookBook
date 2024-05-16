451fe97baaef7c0bba3b77d24ff6fad5e84b66e8
elvleh <elvleh@utu.fi>
2024-05-16 10:36:00 +0300

    recipe form

---
+++ src/components/RecipeForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recipe-form">
+    <form @submit.prevent="submitRecipe">
+      <div class="form-group">
+        <label for="recipeName">Recipe Name:</label>
+        <input type="text" id="recipeName" v-model="recipe.name" required>
+      </div>
+      <div class="form-group">
+        <label for="ingredients">Ingredients:</label>
+        <textarea id="ingredients" v-model="recipe.ingredients" required rows="5"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="steps">Preparation Steps:</label>
+        <textarea id="steps" v-model="recipe.steps" required rows="7"></textarea>
+      </div>
+      <button type="submit">Submit Recipe</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recipe: {
+        name: '',
+        ingredients: '',
+        steps: ''
+      }
+    };
+  },
+  name: 'RecipeForm',
+  methods: {
+    submitRecipe() {
+      console.log("Submitting recipe:", this.recipe);
+      alert('Recipe submitted! Check the console for details.');
+      this.resetForm();
+    },
+    resetForm() {
+      this.recipe = { name: '', ingredients: '', steps: '' };
+    }
+  }
+}
+</script>
