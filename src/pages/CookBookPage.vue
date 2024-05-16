b1fc87fe146d91d0027e63d37b7eb676d1a12cd7
elvleh <elvleh@utu.fi>
2024-05-16 10:36:26 +0300

    added overlay on cookbook

--- src/pages/CookBookPage.vue
+++ src/pages/CookBookPage.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="page">
     <h1>My cookbook</h1>
+    <button @click="overlay_on">Add new recipe</button>
+    <CreateOverlay />
     <h2>My recipes</h2>
+    <div class="recipes">
+      
+    </div>
     <h2>Saved recipes</h2>
+    <div class="saved">
+      
+    </div>
   </div>
 </template>
 
 <script>
+import CreateOverlay from '../components/CreateOverlay'
+
 export default {
   name: 'CookBookPage',
+  components: {
+    CreateOverlay
+  },
+  methods: {
+    overlay_on() {
+      console.log("overlay on")
+      document.querySelector(".overlay").style.display = "block";
+    }
+  }
 };
 </script>
