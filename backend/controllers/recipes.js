fd2121bc662aa85e08ff9627c2f0d46ec4556970
elvleh <elvleh@utu.fi>
2024-05-31 10:46:05 +0300

    separate recipes controller

---
+++ backend/controllers/recipes.js
@@ -0,0 +1,78 @@
+const recipesRouter = require('express').Router()
+const Recipe = require('../models/recipe')
+
+
+// Get all
+recipesRouter.get('/', (request, response) => {
+    Recipe.find({}).then(recipes => {
+        response.json(recipes)
+    })
+})
+
+
+// Get one by id
+recipesRouter.get('/:id', (request, response, next) => {
+    Recipe.findById(request.params.id)
+        .then(recipe => {
+            if (recipe) {
+                response.json(recipe)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+
+// Add new recipe
+recipesRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    if (!body.name || !body.ingredients || !body.steps) {
+        return response.status(400).json({
+            error: 'something is missing'
+        })
+    }
+
+    const recipe = new Recipe({
+        name: body.name,
+        ingredients: body.ingredients,
+        steps: body.steps,
+        public: body.public || false
+    })
+
+    recipe.save()
+        .then(savedRecipe => {
+            response.json(savedRecipe)
+        })
+        .catch(error => next(error))
+})
+
+// Update recipe by id
+recipesRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+
+    const recipe = new Recipe({
+        name: body.name,
+        ingredients: body.ingredients,
+        steps: body.steps,
+        public: body.public || false
+    })
+
+    Recipe.findByIdAndUpdate(request.params.id, recipe, { new: true })
+        .then(updatedRecipe => {
+            response.json(updatedRecipe)
+        })
+        .catch(error => next(error))
+})
+
+// Delete recipe by id
+recipesRouter.delete('/:id', (request, response, next) => {
+    Recipe.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+module.exports = recipesRouter
