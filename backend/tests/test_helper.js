44270b039a6ca33f45a6853ea84fd9b7c514b895
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-31 17:14:44 +0300

    added nonExistingId for testing

--- backend/tests/test_helper.js
+++ backend/tests/test_helper.js
@@ -1,19 +1,27 @@
 const Recipe = require('../models/recipe')
 
 const initialRecipes = [
-    {
-      name: 'Kakku',
-      ingredients: 'maito\nvehnäjauhoja\nsokeria',
-      steps: 'sekoita\npaista',
-      public: false
-    },
-    {
-      name: 'Piirakka',
-      ingredients: 'voita\nvehnäjauhoja\nsuolaa',
-      steps: 'sekoita\npaista',
-      public: true
-    }
-  ];
+  {
+    name: 'Kakku',
+    ingredients: 'maito\nvehnäjauhoja\nsokeria',
+    steps: 'sekoita\npaista',
+    public: false
+  },
+  {
+    name: 'Piirakka',
+    ingredients: 'voita\nvehnäjauhoja\nsuolaa',
+    steps: 'sekoita\npaista',
+    public: true
+  }
+];
+
+const nonExistingId = async () => {
+  const recipe = new Recipe({ name: 'Viallinen' })
+  await recipe.save()
+  await recipe.deleteOne()
+
+  return recipe._id.toString()
+}
 
 const recipesInDb = async () => {
   const recipes = await Recipe.find({})
@@ -21,5 +29,5 @@ const recipesInDb = async () => {
 }
 
 module.exports = {
-  initialRecipes, recipesInDb
+  initialRecipes, nonExistingId, recipesInDb
 }
