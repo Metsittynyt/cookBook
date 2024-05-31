fd19446d929b4cd0afd0f4f7e18db23934241b23
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-05-31 17:15:41 +0300

    added tests and refactored tests into describe blocks

--- backend/tests/recipe_api.test.js
+++ backend/tests/recipe_api.test.js
@@ -10,134 +10,143 @@ const Recipe = require('../models/recipe');
 // Clear the database and add initial recipes before each test
 beforeEach(async () => {
   await Recipe.deleteMany({})
-
-  let recipeObject = new Recipe(helper.initialRecipes[0])
-  await recipeObject.save()
-
-  recipeObject = new Recipe(helper.initialRecipes[1])
-  await recipeObject.save()
+  await Recipe.insertMany(helper.initialRecipes)
 })
 
 
-// Test to check if recipes are returned as JSON
-test('Recipes are returned as json', async () => {
-  await api
-    .get('/api/recipes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+describe('When there is initially some recipes saved', () => {
+  // Recipes are returned as JSON
+  test('Recipes are returned as json', async () => {
+    await api
+      .get('/api/recipes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
+  // All recipes are returned
+  test('All recipes can be fetched', async () => {
+    const response = await helper.recipesInDb()
+    expect(response).toHaveLength(helper.initialRecipes.length)
+  })
 
-// All recipes can be fetced
-test('All recipes can be fetched', async () => {
-  await api
-    .get('/api/recipes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+})
 
-  const response = await helper.recipesInDb()
-  expect(response).toHaveLength(2)
+describe('Viewing of specific recipe.', () => {
+  test('Succeeds with a valid id.', async () => {
+    const recipes = await helper.recipesInDb()
+    const recipeToView = recipes[0]
+
+    const resultRecipe = await api
+      .get(`/api/recipes/${recipeToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(resultRecipe.body).toEqual(recipeToView)
+  })
+
+  test('Fails with statuscode 404 if recipe does not exist.', async () => {
+    const validNonexistingId = await helper.nonExistingId()
+
+    await api
+      .get(`/api/recipes/${validNonexistingId}`)
+      .expect(404)
+  })
+
+  test('fails with status code 400 if id is invalid', async () => {
+    const invalidId = '6a3d6da78079881a82d3466'; 
+  
+    const response = await api
+      .get(`/api/recipes/${invalidId}`)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+  
+    expect(response.body.error).toBe('Invalid ID format');
+  });
 })
 
 
-// Specific recipe can be viewed
-test('Specific recipe can be viewed', async () => {
-  const recipes = await helper.recipesInDb()
-  const recipeToView = recipes[0]
+describe('Addition of a new recipe', () => {
+  test('Succeeds with a valid data.', async () => {
+    const newRecipe = {
+      name: 'Muffinssi',
+      ingredients: 'jtn, jtn',
+      steps: '1. sekoita, 2. paista',
+      public: false
+    };
 
-  const resultRecipe = await api
-    .get(`/api/recipes/${recipeToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/recipes')
+      .send(newRecipe)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  expect(resultRecipe.body).toEqual(recipeToView)
+    const response = await helper.recipesInDb();
+    expect(response).toHaveLength(helper.initialRecipes.length + 1);
+
+    const names = response.map(r => r.name);
+    expect(names).toContain('Muffinssi');
+  });
+
+  test('Fails with statuscode 400 if data is invalid.', async () => {
+    const newRecipe = {
+      name: 'Virheellinen',
+      public: false
+    }
+
+    await api
+      .post('/api/recipes')
+      .send(newRecipe)
+      .expect(400)
+
+    const response = await helper.recipesInDb()
+    expect(response).toHaveLength(helper.initialRecipes.length)
+  })
 })
 
 
-// Test to check if a valid recipe can be added
-test('A valid recipe can be added', async () => {
-  const newRecipe = {
-    name: 'Muffinssi',
-    ingredients: 'jtn, jtn',
-    steps: '1. sekoita, 2. paista',
-    public: false
-  };
+describe('Handling of recipe update', () => {
+  test('Recipe can be updated', async () => {
+    const recipesAtStart = await helper.recipesInDb();
+    const recipeToUpdate = recipesAtStart[0];
 
-  await api
-    .post('/api/recipes')
-    .send(newRecipe)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const modifiedRecipe = {
+      name: 'Updated Kakku',
+      ingredients: 'maito\nvehnäjauhoja\nsokeria\nkananmuna',
+      steps: '1. sekoita\n2. paista',
+      public: true
+    };
 
-  const response = await helper.recipesInDb();
-  expect(response).toHaveLength(helper.initialRecipes.length + 1);
+    await api
+      .put(`/api/recipes/${recipeToUpdate.id}`)
+      .send(modifiedRecipe)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
 
-  const names = response.map(r => r.name);
-  expect(names).toContain('Muffinssi');
-});
+    const recipesAtEnd = await helper.recipesInDb();
+    const updatedRecipe = recipesAtEnd.find(r => r.id === recipeToUpdate.id);
 
-
-// Test to check that invalid recipe cannot be added
-test('Empty recipe cant be added', async () => {
-  const newRecipe = {
-    name: 'Virheellinen',
-    public: false
-  }
-
-  await api
-    .post('/api/recipes')
-    .send(newRecipe)
-    .expect(400)
-
-  const response = await helper.recipesInDb()
-  expect(response).toHaveLength(helper.initialRecipes.length)
+    expect(updatedRecipe.name).toBe(modifiedRecipe.name);
+    expect(updatedRecipe.ingredients).toBe(modifiedRecipe.ingredients);
+    expect(updatedRecipe.steps).toBe(modifiedRecipe.steps);
+    expect(updatedRecipe.public).toBe(modifiedRecipe.public);
+  });
 })
 
 
-// Recipe can be updated
-test('Recipe can be updated', async () => {
-  // Get all recipes from the database
-  const recipesAtStart = await helper.recipesInDb();
-  const recipeToUpdate = recipesAtStart[0];
+describe('Deletion of recipe.', () => {
+  test('Recipe can be deleted', async () => {
+    const recipes = await helper.recipesInDb()
+    const recipeToDelete = recipes[0]
 
-  // New data to update the recipe
-  const modifiedRecipe = {
-    name: 'Updated Kakku',
-    ingredients: 'maito\nvehnäjauhoja\nsokeria\nkananmuna',
-    steps: '1. sekoita\n2. paista',
-    public: true
-  };
+    await api
+      .delete(`/api/recipes/${recipeToDelete.id}`)
+      .expect(204)
 
-  // Make the PUT request to update the recipe
-  await api
-    .put(`/api/recipes/${recipeToUpdate.id}`)
-    .send(modifiedRecipe)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
-  // Get all recipes from the database after the update
-  const recipesAtEnd = await helper.recipesInDb();
-  const updatedRecipe = recipesAtEnd.find(r => r.id === recipeToUpdate.id);
-
-  // Verify the update
-  expect(updatedRecipe.name).toBe(modifiedRecipe.name);
-  expect(updatedRecipe.ingredients).toBe(modifiedRecipe.ingredients);
-  expect(updatedRecipe.steps).toBe(modifiedRecipe.steps);
-  expect(updatedRecipe.public).toBe(modifiedRecipe.public);
-});
-
-// Recipe can be deleted
-test('Recipe can be deleted', async () => {
-  const recipes = await helper.recipesInDb()
-  const recipeToDelete = recipes[0]
-
-  await api
-    .delete(`/api/recipes/${recipeToDelete.id}`)
-    .expect(204)
-
-  const response = await helper.recipesInDb()
-  expect(response).toHaveLength(helper.initialRecipes.length - 1)
+    const response = await helper.recipesInDb()
+    expect(response).toHaveLength(helper.initialRecipes.length - 1)
+  })
 })
+
 
 // Close the database connection after all tests
 afterAll(async () => {
