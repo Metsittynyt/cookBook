f5a751179fa0b3db1902f33623493cba10bf0b52
elvleh <elvleh@utu.fi>
2024-06-04 12:37:46 +0300

    use uniqueValidator on user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   name: String,
   passwordHash: String,
-  notes: [
+  recipes: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Recipe'
     }
   ],
 })
+
+userSchema.plugin(uniqueValidator)
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
