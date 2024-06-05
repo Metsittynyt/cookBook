33225e8dff13049a3e33f6d73c9470fe49ab6eb5
elvleh <elvleh@utu.fi>
2024-06-05 13:12:05 +0300

    handle user log out

--- frontend/src/pages/UserPage.vue
+++ frontend/src/pages/UserPage.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="page">
-    <h1>Log in</h1>
-    <LogInForm />
+    <h1 v-if="!isLoggedIn">Log in</h1>
+    <LogInForm v-if="!isLoggedIn" />
+
+    <div v-else>
+      <h1>Logged in as {{ username }}</h1>
+      <button @click="logout" class="logout-button">Log out</button>
+    </div>
   </div>
 </template>
+
 
 <script>
 import LogInForm from '../components/LogInForm'
@@ -12,6 +18,30 @@ export default {
   name: 'UserPage',
   components: {
     LogInForm
+  },
+  data() {
+    return {
+      username: '' // This will store the username
+    }
+  },
+  computed: {
+    isLoggedIn() {
+      // Check if the user is logged in by checking for a token in local storage
+      return !!localStorage.getItem('token');
+    }
+  },
+  created() {
+    // Optionally, retrieve the username from local storage if available
+    this.username = localStorage.getItem('username') || 'User';
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('username'); // Clear the username from local storage
+      this.$router.push('/').then(() => {
+        window.location.reload();
+      });
+    }
   }
-};
+}
 </script>
