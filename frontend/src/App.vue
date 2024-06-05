5076fd0245ab8dd749472eb607cdda4890c2ea67
elvleh <elvleh@utu.fi>
2024-06-05 13:15:57 +0300

    add banners to pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,29 @@
 <template>
-    <TopNav />
-    <router-view/>
-    <BottomFooter />
+  <TopNav />
+  <PageBanner :bannerClass="bannerClass" />
+  <router-view />
+  <BottomFooter />
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import TopNav from './components/TopNav.vue'
+import PageBanner from './components/PageBanner.vue'
 import BottomFooter from './components/BottomFooter.vue'
 
 export default {
   name: 'App',
   components: {
     TopNav,
+    PageBanner,
     BottomFooter
+  },
+  setup() {
+    const route = useRoute();
+    const bannerClass = computed(() => route.meta.bannerClass || '');
+
+    return { bannerClass };
   }
 }
 </script>
