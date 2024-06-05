cf172cc0eaef34dd3f767ff822dddb55fac55515
elvleh <elvleh@utu.fi>
2024-06-05 13:14:30 +0300

    page banner component

---
+++ frontend/src/components/PageBanner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :class="['banner', bannerClass]"></div>
+</template>
+
+<script>
+export default {
+    name: 'PageBanner',
+    props: {
+        bannerClass: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style>
+.banner {
+    height: 200px;
+    width: 100%;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.home-banner {
+    /*background-image: url('');*/
+    background-color: var(--lightBlue);
+}
+
+
+</style>
