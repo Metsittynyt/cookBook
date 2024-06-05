dd576530d9293ee55fecf2c3071ce345fcb71f35
elvleh <elvleh@utu.fi>
2024-06-05 13:12:46 +0300

    dont show recipes page when user is not logged in

--- frontend/src/components/TopNav.vue
+++ frontend/src/components/TopNav.vue
@@ -6,17 +6,22 @@
       </router-link>
     </div>
     <div class="topnavLinks">
-      <router-link to="/" exact class="nav-item" active-class="active">CookBook</router-link>
-      <router-link to="/cookbook" class="nav-item" active-class="active">My recipes</router-link>
       <router-link to="/user" class="nav-item" active-class="active">
         <i class="fas fa-user-alt" id="user_icon"></i>
       </router-link>
+      <router-link to="/cookbook" v-if="isLoggedIn" class="nav-item" active-class="active">My recipes</router-link>
+      <router-link to="/" exact class="nav-item" active-class="active">CookBook</router-link>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TopNav'
+  name: 'TopNav',
+  computed: {
+    isLoggedIn() {
+      return !!localStorage.getItem('token');
+    }
+  }
 }
 </script>
