fbd55e46a0c18ec12a63b129aea112e78655f9ef
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-06-11 16:42:33 +0300

    keep old version for my recipes page

---
+++ frontend/src/components/MyRecipeBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="my-recipe">
+        <h3>{{ recipe.name }}</h3>
+        <div id="ingredients">
+            <h4>Ingredients:</h4>
+            <ul>
+                <li v-for="ingredient in splitText(recipe.ingredients)" :key="ingredient">{{ ingredient }}</li>
+            </ul>
+        </div>
+        <div id="steps">
+            <h4>Steps:</h4>
+            <ol>
+                <li v-for="step in splitText(recipe.steps)" :key="step">{{ step }}</li>
+            </ol>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyRecipeBox',
+    props: {
+        recipe: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        splitText(text) {
+            return text.split('\n');
+        }
+    }
+}
+</script>
