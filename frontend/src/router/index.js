4b377483d03ca741c2df6bdc97281d94f54beb95
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-06-11 16:40:49 +0300

    add dynamic router for recipe page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
+import RecipePage from '../pages/RecipePage.vue';
 import CookBookPage from '../pages/CookBookPage.vue';
 import UserPage from '../pages/UserPage.vue';
 
@@ -7,6 +8,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage, name: 'HomePage', meta: { bannerClass: 'home-banner' } },
+    { path: '/:id', component: RecipePage, name: 'RecipePage', props: true, meta: { bannerClass: 'recipe-banner' } },
     { path: '/cookbook', component: CookBookPage, name: 'CookBookPage', meta: { bannerClass: 'cookbook-banner' } },
     { path: '/user', component: UserPage, name: 'UserPage', meta: { bannerClass: 'user-banner' } }
     // other routes
