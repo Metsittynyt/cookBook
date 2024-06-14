2fbe98388ccad5dbcef3dfc79e2020a32bffed8d
elvleh <elvleh@utu.fi>
2024-06-14 13:22:20 +0300

    get username

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,18 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const isAuthenticated = require('../utils/auth');
 
 usersRouter.get('/', async (request, response) => {
     const users = await User.find({}).populate('recipes', {name: 1, public: 1})
     response.json(users)
 })
+
+usersRouter.get('/name',isAuthenticated, async (request, response) => {
+    const userId = request.user.id;
+    const name = await User.findById(userId)
+    response.json(name.username)
+});
 
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
