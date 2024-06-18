80b717fc2ebc3a83a9a42e3db7f564c7155a1af1
elvleh <elvleh@utu.fi>
2024-06-18 11:28:01 +0300

    remove overlay

--- frontend/src/pages/CookBookPage.vue
+++ frontend/src/pages/CookBookPage.vue
@@ -2,13 +2,10 @@
   <div>
     <h1>My cookbook</h1>
     <button @click="add_recipe">Add new recipe</button>
-    <CreateOverlay v-if="showOverlay" :initialRecipe="selectedRecipe" @close-overlay="closeOverlay"
-      @submit-success="handleSuccess" />
     <MyRecipes :recipes="myRecipes" @edit-recipe="edit_recipe" @change="fetchRecipes" />
     <h2>Saved recipes</h2>
     <p v-if="savedRecipes.length <= 0">Your saved recipes will appear here. Start by exploring the wide range of options
-      and save the ones you love!
-    </p>
+      and save the ones you love!</p>
     <div class="RecipesGrid">
       <div v-for="recipe in savedRecipes" :key="recipe.id" class="recipe">
         <RecipeBox :recipe="recipe" @update:recipe="handleRecipeUpdate" />
@@ -19,21 +16,17 @@
 
 <script>
 import recipeService from '@/services/recipes'
-import CreateOverlay from '../components/CreateOverlay'
 import MyRecipes from '../components/MyRecipes'
 import RecipeBox from '@/components/RecipeBox.vue';
 
 export default {
   name: 'CookBookPage',
   components: {
-    CreateOverlay,
     MyRecipes,
     RecipeBox
   },
   data() {
     return {
-      showOverlay: false,
-      selectedRecipe: null,
       myRecipes: [],
       savedRecipes: []
     }
@@ -44,25 +37,10 @@ export default {
       this.savedRecipes = await recipeService.getSome(false, false, true)
     },
     add_recipe() {
-      console.log("Add new recipe...")
-      this.selectedRecipe = {
-        name: '',
-        ingredients: '',
-        steps: '',
-        public: false
-      };
-      this.showOverlay = true;
+      this.$router.push('/create');
     },
     edit_recipe(recipe) {
-      this.selectedRecipe = recipe;
-      this.showOverlay = true;
-    },
-    closeOverlay() {
-      this.showOverlay = false;
-    },
-    handleSuccess() {
-      this.closeOverlay();
-      this.fetchRecipes(); // Fetch recipes after successful submit
+      this.$router.push(`/edit/${recipe.id}`);
     },
     handleRecipeUpdate() {
       this.fetchRecipes()
