57072cf006fcb078cf71a623c14514493af1b708
elvleh <elvleh@utu.fi>
2024-06-18 11:27:12 +0300

    add routes to edit and create pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '../pages/HomePage.vue';
 import RecipePage from '../pages/RecipePage.vue';
 import CookBookPage from '../pages/CookBookPage.vue';
 import UserPage from '../pages/UserPage.vue';
+import CreateEditPage from '@/pages/CreateEditPage.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,7 +11,9 @@ const router = createRouter({
     { path: '/', component: HomePage, name: 'HomePage', meta: { bannerClass: 'home-banner' } },
     { path: '/:id', component: RecipePage, name: 'RecipePage', props: true, meta: { bannerClass: 'recipe-banner' } },
     { path: '/cookbook', component: CookBookPage, name: 'CookBookPage', meta: { bannerClass: 'cookbook-banner' } },
-    { path: '/user', component: UserPage, name: 'UserPage', meta: { bannerClass: 'user-banner' } }
+    { path: '/user', component: UserPage, name: 'UserPage', meta: { bannerClass: 'user-banner' } },
+    { path: '/create', component: CreateEditPage, name: 'CreateRecipePage', meta: { bannerClass: 'create-banner' } },
+    { path: '/edit/:id', component: CreateEditPage, name: 'EditRecipePage', props: true, meta: { bannerClass: 'edit-banner' } }
     // other routes
   ],
 });
