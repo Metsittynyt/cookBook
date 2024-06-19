d5b5bf13cd16aac66131e87d1388753fd163456a
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2024-06-19 19:45:52 +0300

    add route to get user by id

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,17 +1,33 @@
 const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 const isAuthenticated = require('../utils/auth');
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('recipes', {name: 1, public: 1})
+    const users = await User.find({}).populate('recipes', { name: 1, public: 1 })
     response.json(users)
 })
 
-usersRouter.get('/name',isAuthenticated, async (request, response) => {
+usersRouter.get('/name', isAuthenticated, async (request, response) => {
     const userId = request.user.id;
     const name = await User.findById(userId)
     response.json(name.username)
+});
+
+usersRouter.get('/:id', async (request, response) => {
+    const { id } = request.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return response.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const user = await User.findById(id);
+    if (user) {
+        response.json(user);
+    } else {
+        response.status(404).json({ error: 'Recipe not found' });
+    }
 });
 
 usersRouter.post('/', async (request, response) => {
