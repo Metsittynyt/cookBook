6aad8e32036de242a5eda6f19218c6d903ec2371
elvleh <elvleh@utu.fi>
2024-07-29 10:12:03 +0300

    handle also success messages

---
+++ frontend/src/components/MessageComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div :class="['message', messageType]" v-if="messageObject">
+        {{ messageObject.text }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MessageComponent',
+    props: {
+        messageObject: {
+            type: Object,
+            required: true,
+            validator: function (value) {
+                // eslint-disable-next-line no-prototype-builtins
+                return value.hasOwnProperty('text') && value.hasOwnProperty('type');
+            }
+        }
+    },
+    computed: {
+        messageType() {
+            return this.messageObject.type === 'error' ? 'errorMessage' : 'successMessage';
+        }
+    }
+}
+</script>
+
+<style>
+.message {
+    color: black;
+    padding: 10px;
+    border-radius: 10px;
+    font-size: 20px;
+    width: fit-content;
+    margin: auto;
+    margin-top: 10px;
+}
+
+.errorMessage {
+    border: 1px solid #E29578;
+    background-color: #FFDDD2;
+}
+
+.successMessage {
+    border: 1px solid #006D77;
+    background-color: #83C5BE;
+}
+</style>
